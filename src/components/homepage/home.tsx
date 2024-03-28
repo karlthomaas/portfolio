69424d988632c47d60a6212cafe3ea3cb34c45d0
Karl-Thomas <karl-thomas@zink.ee>
2024-03-28 15:35:59 +0200

    Update homepage layout

--- src/components/homepage/home.tsx
+++ src/components/homepage/home.tsx
@@ -10,7 +10,7 @@ export const HomeSection = () => {
     <HomeLayoutWrapper id="home" tag='section'>
       <div className='h-[calc(100vh-16px)] text-foreground'>
         <div className='flex h-max w-full flex-col pt-[200px] sm:flex-row sm:justify-between'>
-          <div className='my-auto'>
+          <div className='my-auto w-max'>
             <h2 className='text-center text-3xl text-foreground/90 sm:mt-1 sm:text-left md:text-4xl lg:text-5xl'>Full-Stack Developer</h2>
             <h3 className='mt-2 text-center text-lg sm:text-left md:text-xl lg:text-2xl'>Hey, I'm Karl-Thomas Zink from Estonia! </h3>
             <div className='mt-5 flex justify-center space-x-3 sm:justify-normal'>
@@ -18,7 +18,7 @@ export const HomeSection = () => {
               <FaLinkedin className='text-4xl text-foreground' />
             </div>
           </div>
-          <div className='order-first mx-auto mb-8 sm:order-last'>
+          <div className='order-first mb-8 sm:order-last'>
             <BackgroundGradient className='rounded-[22px] p-2 '>
               <div className='h-[270px] w-[270px] rounded-3xl bg-background ' />
             </BackgroundGradient>
