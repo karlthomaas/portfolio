d7e7dc6200c960e4768994c476ae1e8ca64d7c13
Karl-Thomas <karl-thomas@zink.ee>
2024-03-28 15:36:06 +0200

    Update floating navbar styles

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -61,7 +61,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "hidden md:flex max-w-screen-lg w-full fixed top-10 inset-x-0 mx-auto border border-border dark:border-white/[0.9] rounded-xl dark:bg-black bg-card shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-4  items-center justify-between",
+          "hidden md:flex max-w-screen-lg w-full fixed top-10 inset-x-0 mx-auto border border-border dark:border-white/[0.9] rounded-xl backdrop-blur-md shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-4  items-center justify-evenly",
           className
         )}
       >
@@ -74,7 +74,7 @@ export const FloatingNav = ({
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            <span className="hidden sm:block text-sm sm:text-md">{navItem.name}</span>
           </a>
         ))}
       </motion.div>
