6c12dba2d942cc90973ae1f0b63fabc338a164f0
Karl-Thomas <karl-thomas@zink.ee>
2024-03-29 13:50:09 +0200

    Update about section in homepage

--- src/components/homepage/about.tsx
+++ src/components/homepage/about.tsx
@@ -1,22 +1,26 @@
 import { HomeLayoutWrapper } from '..';
-
 export const AboutSection = () => {
   return (
-    <div className='h-full w-full bg-[#060D1F] py-20'>
-      <HomeLayoutWrapper tag='section' id='about' className='flex space-x-10'>
-        <div className='my-auto hidden h-[250px] w-[250px] flex-none rounded-3xl bg-slate-500 sm:block'></div>
+    <div className='bg-[#080f1e] mb-10 h-full w-full  py-24'>
+      <HomeLayoutWrapper tag='section' id='about' className='flex md:space-x-10'>
+        <img src="/images/programming.jpeg" className='object-cover my-auto hidden h-[310px] w-[310px] flex-none rounded-3xl bg-slate-500 md:block'></img>
 
-        <div className='my-auto'>
+        <div className='my-auto -mt-10'>
           <h1 className='mb-4 text-3xl font-semibold sm:text-2xl'>About me</h1>
-          <p className='leading-relaxed tracking-wide'>
-            I've been really passionate about programming since beginning of High school.
+          <p className='text-pretty text-justify leading-relaxed tracking-wide'>
+            Hey, I'm Karl-Thomas, a <span className='text-primary'>passionate programmer</span> who got into coding back in high school.
             <br />
             <br />
-            My Journey started with creating Discord bots for fun. Then I put my skills in use and started freelancing bots and automations
-            for small bussinesses and communities.
+            My journey started with taking Python courses in High school. As I got delved deeper into Python, I discovered it's potential
+            and started creating discord bots and web automations for fun. My passsion for programming opened the doors for{' '}
+            <span className='text-primary'>freelancing opportunities</span>.
             <br />
             <br />
-            Currently I am freelancing full-stack web development and studying in Kood/Jõhvi!
+            I'm primarly self-taught, but thirst for knowledge, collaborations and feedback led me to pursue formal studies at{' '}
+            <span className='text-primary'>Kood/Jõhvi</span> in 2023.
+            <br />
+            <br />
+            Today, I'm proud to be a part-time fullstack developer!
           </p>
         </div>
       </HomeLayoutWrapper>
