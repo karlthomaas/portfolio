ba58bb1c193997fae674e994dd5c59910b7645da
Karl-Thomas <karl-thomas@zink.ee>
2024-03-29 13:49:52 +0200

    Refactor project showcase component

--- src/components/homepage/project-showcase.tsx
+++ src/components/homepage/project-showcase.tsx
@@ -1,34 +1,87 @@
-import { ReactNode } from "react";
-import { cn } from "@/lib/utils";
+import { ReactNode } from 'react';
+import { cn } from '@/lib/utils';
+import { ArrowDownIcon } from 'lucide-react';
+import { Button } from '..';
+import { ExternalLink } from 'lucide-react';
 
 export const ProjectShowcase = ({
+  id,
   name,
   className,
   description,
   link,
-  image,
   stack,
+  activeProject,
+  setActiveProject,
 }: {
+  id: string;
   name: string;
-  className: string | undefined;
-  description: string;
-  link: string;
-  image: string | null;
-  stack: ReactNode[];
+  className?: string;
+  description?: string;
+  link?: string;
+  stack?: ReactNode[];
+  activeProject?: string | null;
+  setActiveProject: (id: string | null) => void;
 }) => {
   return (
-    <div className={cn(`flex h-[200px] w-full flex-col rounded-lg bg-background border-[1px] border-border p-4 text-foreground transition-all ease-in duration-200 md:h-[350px]`, className)}>
+    <div
+      className={cn(
+        `flex h-[140px] w-full rounded-lg border-[1px] border-border bg-background p-4 
+        text-foreground transition-all duration-200 ease-in-out md:h-[300px]`,
+        className,
+        {
+          'h-[300px]': activeProject === id,
+        }
+      )}
+    >
       <div className='my-auto flex h-[90%] w-full space-x-3'>
-        {/* <div className='my-auto aspect-square h-full rounded-lg bg-foreground/60' /> */}
-        <div className="flex flex-col space-y-2">
-        <h1 className='text-2xl font-medium tracking-wide'>{name}</h1>
-        <p>{description}</p>
-        <ul className="flex space-x-2">
-            {stack.map((item, index) => (
-                <li key={index}>{item}{link}{image}</li>
-            ))}
-        </ul>
+        <div className='flex w-full flex-col justify-between'>
+          <div className='flex w-full items-center md:justify-between'>
+            <h1 className='pl-2 text-xl font-medium tracking-wide'>{name}</h1>
+            {link && (
+              <a className='order-first md:order-last' href={link} target='_blank'>
+                <Button className='my-auto' variant='ghost' size='sm'>
+                  <ExternalLink />
+                </Button>
+              </a>
+            )}
+          </div>
+          <p
+            className={cn('px-1 py-5', {
+              'hidden text-pretty md:block': activeProject !== id,
+            })}
+          >
+            {description}
+          </p>
+          {stack && (
+            <ul className='flex w-max space-x-5 rounded-full bg-primary/20 px-2 py-1'>
+              {stack.map((item, index) => (
+                <li className='my-auto' key={index}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
+      </div>
+      <div className='my-auto md:hidden'>
+        <Button
+          size='icon'
+          variant='ghost'
+          onClick={() => {
+            if (activeProject === id) {
+              setActiveProject(null);
+            } else {
+              setActiveProject(id);
+            }
+          }}
+        >
+          <ArrowDownIcon
+            className={cn('h-6 w-6 transition-transform duration-300', {
+              'rotate-180 transform': activeProject === id,
+            })}
+          />
+        </Button>
       </div>
     </div>
   );
