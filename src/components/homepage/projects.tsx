fcb9a15b619574efe3a3df194a8e5102b3534de1
Karl-Thomas <karl-thomas@zink.ee>
2024-03-29 13:49:48 +0200

    Add new project icons and update project descriptions

--- src/components/homepage/projects.tsx
+++ src/components/homepage/projects.tsx
@@ -1,65 +1,82 @@
 import { HomeLayoutWrapper, ProjectShowcase } from '..';
-import { BiLogoTypescript } from 'react-icons/bi';
-import { SiNextdotjs, SiShopify, SiTailwindcss } from 'react-icons/si';
-import { GrGraphQl } from 'react-icons/gr';
+
 import NextJs from '/icons/nextjs-icon.svg';
 import Typescript from '/icons/typescript-icon.svg';
 import Shopify from '/icons/shopify.svg';
 import GraphQL from '/icons/graphql.svg';
 import Tailwind from '/icons/tailwindcss-icon.svg';
+import Python from '/icons/python.svg';
+import Discord from '/icons/discord-icon.svg';
+import Go from '/icons/go.svg';
+import Docker from '/icons/docker-icon.svg';
+import Astro from '/icons/astro-icon.svg';
+import { useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export const ProjectsSection = () => {
+  const [activeProject, setActiveProject] = useState<string | null>(null);
+
   return (
-    <div>
-      <HomeLayoutWrapper tag='section' id='projects'>
-        <h1 className='mb-4 mt-10 text-2xl font-medium sm:text-2xl'>My Projects</h1>
+    <div className='mb-24'>
+      <HomeLayoutWrapper tag='section' id='projects' className='mb-10 mt-10'>
+        <h1 className='mb-4 mt-10 text-2xl font-medium'>My Projects</h1>
         <section className='grid grid-cols-1 gap-5 md:grid-cols-2'>
           <ProjectShowcase
+            id='pilt_tasku'
             name='Pilt tasku'
-            description='Surveillance equipment e-commerce store. '
+            description='Surveillance equipment e-commerce store. For this project I used headless Shopify with NextJS, because that allowed me to
+            create more faster and flexible website for the client.'
             stack={[
-              <img src={NextJs} className='aspect-square w-[40px]' />,
-              <img src={Typescript} className='aspect-square w-[40px]' />,
-              <img src={GraphQL} className='aspect-square w-[40px]' />,
-              <img src={Shopify} className='aspect-square w-[40px]' />,
-              <img src={Tailwind} className='aspect-square w-[40px]' />,
+              <StackIcon src={NextJs} className='w-[40px]' />,
+              <StackIcon src={Typescript} className='w-[33px]' />,
+              <StackIcon src={GraphQL} />,
+              <StackIcon src={Shopify} />,
+              <StackIcon src={Tailwind} />,
             ]}
+            activeProject={activeProject}
+            setActiveProject={setActiveProject}
+            link='https://pilttasku.ee'
           />
           <ProjectShowcase
-            name='Pilt tasku'
-            description='Surveillance equipment e-commerce store. '
+            id='social_network'
+            name='Social Network'
+            description='School project where we had to build Social media platform. For frontend we used NextJs and for backend we used Go and everything 
+            is containerized with Docker.'
             stack={[
-              <SiNextdotjs className='text-4xl text-black' />,
-              <BiLogoTypescript className='text-4xl text-blue-600' />,
-              <GrGraphQl className='text-4xl text-pink-600' />,
-              <SiShopify className='text-4xl text-green-600' />,
-              <SiTailwindcss className='text-4xl text-cyan-600' />,
+              <StackIcon src={NextJs} className='w-[40px]' />,
+              <StackIcon src={Typescript} className='w-[33px]' />,
+              <StackIcon src={Go} />,
+              <StackIcon src={Docker} />,
+              <StackIcon src={Tailwind} />,
             ]}
+            activeProject={activeProject}
+            setActiveProject={setActiveProject}
+            link='https://kettad.ee/'
           />
           <ProjectShowcase
-            name='Pilt tasku'
-            description='Surveillance equipment e-commerce store. '
-            stack={[
-              <SiNextdotjs className='text-4xl text-black' />,
-              <BiLogoTypescript className='text-4xl text-blue-600' />,
-              <GrGraphQl className='text-4xl text-pink-600' />,
-              <SiShopify className='text-4xl text-green-600' />,
-              <SiTailwindcss className='text-4xl text-cyan-600' />,
-            ]}
+            id='kettad'
+            name='Kettad'
+            description='Discgolf discs website. Users can find discs by name or characteristics. Every disc contains detailed information and flying trajectory'
+            stack={[<StackIcon src={Astro} className='w-[30px]'/>, <StackIcon src={Tailwind} />]}
+            activeProject={activeProject}
+            setActiveProject={setActiveProject}
+            link='https://github.com/karlthomaas/Kalev-Discord-Bot'
           />
           <ProjectShowcase
+            id='kalevi_bot'
             name='Kalevi bot'
-            description='Open source Discord bot for Estonian community. '
-            stack={[
-              <SiNextdotjs className='text-4xl text-black' />,
-              <BiLogoTypescript className='text-4xl text-blue-600' />,
-              <GrGraphQl className='text-4xl text-pink-600' />,
-              <SiShopify className='text-4xl text-green-600' />,
-              <SiTailwindcss className='text-4xl text-cyan-600' />,
-            ]}
+            description='Discord bot for Estonian communities. Has many features like listening to music, search wikipedia, and more.'
+            stack={[<StackIcon src={Python} />, <StackIcon src={Discord} />]}
+            activeProject={activeProject}
+            setActiveProject={setActiveProject}
+            link='https://github.com/karlthomaas/Kalev-Discord-Bot'
           />
         </section>
       </HomeLayoutWrapper>
     </div>
   );
+};
+
+const StackIcon = ({ src, className }: { src: string; className?: string }) => {
+  return <img src={src} className={cn('aspect-square w-[35px] transition-transform duration-200 hover:scale-105', className)} />;
 };
