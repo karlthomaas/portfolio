5f08fd9b7b583af0848114501a95a7352d188e95
Karl-Thomas <karl-thomas@zink.ee>
2024-03-29 13:50:02 +0200

    Refactor contact-me component

--- src/components/homepage/contact-me.tsx
+++ src/components/homepage/contact-me.tsx
@@ -1,14 +1,22 @@
-import { HomeLayoutWrapper } from "..";
-import { Button } from "..";
+import { HomeLayoutWrapper } from '..';
+import { Button } from '..';
 
 export const ContactMe = () => {
   return (
-    <HomeLayoutWrapper tag='section' id='contact' className='mt-16'>
-      {/* <div className='h-max min-h-[400px] w-full border-border bg-white rounded-lg'></div> */}
-      <h1 className="text-2xl">Want to collab and make something awesome?</h1>
-      <a href="mailto:karl-thomas@zink.ee">
-        <Button>Contact me</Button>
-      </a>
-    </HomeLayoutWrapper>
+    <div className='bg-gradient-to-r from-[#080f1e] to-[#0c162c] mt-10 py-14'>
+      <HomeLayoutWrapper tag='section' id='contact'>
+        <div className='mx-auto flex h-max w-full flex-col justify-center my-auto space-y-5  rounded-xl p-5 text-foreground '>
+          <h1 className='text-3xl sm:text-3xl'>Ready to collaborate?</h1>
+          <p className='text-foreground/75 sm:text-lg'>
+            Interested in collaborating or have a question? I'd love to hear from you! Click the button below and send me an email!
+          </p>
+          <a href='mailto:karl-thomas@zink.ee' className='w-max'>
+            <Button size='lg' className='text-white transition-transform ease-in-out hover:-translate-y-1 sm:text-lg'>
+              Click to Connect
+            </Button>
+          </a>
+        </div>
+      </HomeLayoutWrapper>
+    </div>
   );
 };
