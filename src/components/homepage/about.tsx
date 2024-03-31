ee3ccc76db817e49a26f40d25cb096264184fe22
Karl-Thomas <karl-thomas@zink.ee>
2024-03-31 18:23:32 +0300

    fix grammatic mistakes

--- src/components/homepage/about.tsx
+++ src/components/homepage/about.tsx
@@ -15,16 +15,16 @@ export const AboutSection = () => {
             Hey, I'm Karl-Thomas, a <span className='text-primary'>passionate programmer</span> who got into coding back in high school.
             <br />
             <br />
-            My journey started with taking Python courses in High school. As I got delved deeper into Python, I discovered it's potential
-            and started creating discord bots and web automations for fun. My passsion for programming opened the doors for{' '}
+            My journey started with taking Python courses in High school. As I delved deeper into Python, I discovered its potential
+            and started creating discord bots and web automation for fun. My passion for programming opened the doors for{' '}
             <span className='text-primary'>freelancing opportunities</span>.
             <br />
             <br />
-            I'm primarly self-taught, but thirst for knowledge, collaborations and feedback led me to pursue formal studies at{' '}
+            I'm primarily self-taught, but my thirst for knowledge, collaboration and feedback led me to pursue formal studies at{' '}
             <span className='text-primary'>Kood/Jõhvi</span> in 2023.
             <br />
             <br />
-            Today, I'm proud to be a part-time fullstack developer!
+            Today, I'm proud to be a part-time full-stack developer!
           </p>
         </div>
       </HomeLayoutWrapper>
