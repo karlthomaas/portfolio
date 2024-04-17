70b5708210b477f5e9aea5d59fbf2c2af7848b8f
Karl-Thomas <karl-thomas@zink.ee>
2024-04-17 17:41:23 +0300

    Add lineClamp utility to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,9 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      lineClamp: {
+        8: '8',
+      }
     },
   },
   plugins: [require('tailwindcss-animate'), addVariablesForColors],
